200c461852539daa3d63a42719526d287c945b43
MNdagu <ndagumark@gmail.com>
2024-07-17 12:04:14 +0300

    feat:reminder countdown

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
 const current_date = document.querySelector('.current-date');
-const previous = document.getElementById('prev');
-const next = document.getElementById('next');
 const prenexIcons = document.querySelectorAll('.navigation span');
 const day = document.querySelector('.dates');
 const event_date = document.getElementById('event-date');
 const event_description = document.getElementById('event-description');
+const event_name = document.getElementById('event-name')
 const event_reminder = document.getElementById('event-reminder');
 const event_input = document.getElementById('event-input')
+const event_description_input = document.getElementById('event-description-input')
 const form = document.getElementById('form')
 
 let date = new Date();
 let year = date.getFullYear();
 let month = date.getMonth();
+
 let events = [];
 
 
@@ -85,23 +90,57 @@ day.addEventListener('click', (e) => {
     let date = e.target
 
     event_date.textContent = `${months[month]} ${date.textContent},  ${year} `
-    event_description.textContent = 'no event'
+    event_name.textContent = 'no event'
+    event_description.textContent = 'no event description'
     event_reminder.textContent = 'no reminder set'
 
 
 })
 
+// let countdown_month = document.getElementById('countdown-month').value;
 form.addEventListener('submit', (e) => {
+    let countdown_date = Number(document.getElementById('countdown-date').value);
+    let countdown = Number(new Date(`${month + 1} ${countdown_date} ${year}`).getTime());
+let x = setInterval(function () {
+    let now = new Date().getTime();
+    let distance = countdown - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    let displayCountdown = `${days}d ${hours}h ${minutes}m ${seconds}s`
+    
+    if (countdown_date){
+        event_reminder.textContent = displayCountdown;
+    }else{
+        event_reminder.textContent = 'no set reminder'
+    }
+
+    if (distance < 0){
+        clearInterval(x);
+        event_reminder.textContent = 'EXPIRED'
+
+    }
+},1000)
 
     let newEvent = event_input.value;
+    let newDescription = event_description_input.value;
     if (newEvent) {
-        event_description.textContent = newEvent;
-        event_reminder.textContent = 'reminder'
-        events.push(newEvent)
+        event_description.textContent = newDescription;
+        event_name.textContent = newEvent;
+       // event_reminder.textContent = displayCountdown;
+        events.push(newEvent);
+        console.log(events)
 
     }
     e.preventDefault();
     form.reset();
     document.querySelector('.modal').style.display = 'none'
 
+
 })
+})
+
+
